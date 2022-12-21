16faff59bab0f690b804a5c51cf7e72a80e6abb0
shadi <15.7.99sh@gmail.com>
2022-12-21 20:28:50 +0200

    CRUD is now available for Campaign

---
+++ campaigns/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from .models import Campaign
+
+class CampaignForm(ModelForm):
+    class Meta:
+        model = Campaign
+        fields = ['title', 'description', 'start_date', 'end_date']
--- campaigns/views.py
+++ campaigns/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from .forms import CampaignForm
 #from .models import Participant
 from .models import Campaign
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, redirect
 # Create your views here.
 
 def list_of_campaigns(request):
@@ -11,5 +12,34 @@ def list_of_campaigns(request):
 
 def campaign_details(request, campaign_id):
     campaign = get_object_or_404(Campaign, id=campaign_id)
-    context = {'campaign': campaign}
-    return render(request, 'campaigns/campaign_details.html', context)
+    
+    if request.method == 'GET':
+        form = CampaignForm(instance=campaign)
+        context =  {'form': form, 'campaign': campaign}
+        return render(request, 'campaigns/campaign_details.html', context)
+    else:
+        try:
+            form = CampaignForm(request.POST, instance=campaign)
+            form.save()
+            return redirect('campaigns:list_of_campaigns')
+        except ValueError:
+            context = {'form': CampaignForm(), 'error': 'Bad data try again'}
+            return render(request, 'campaigns/campaign_details.html', context)
+    
+
+def campaign_create(request):
+    if request.method == 'GET':
+        return render(request, 'campaigns/campaign_form.html', {'form': CampaignForm()})
+    else:
+        try:
+            form = CampaignForm(request.POST)
+            form.save()
+            return redirect('campaigns:list_of_campaigns')
+        except ValueError:
+            context = {'form': CampaignForm(), 'error': 'Bad data try again'}
+            return render(request, 'campaigns/campaign_form.html', context)
+        
+def campaign_delete(request, campaign_id):
+    campaign = get_object_or_404(Campaign, id=campaign_id)
+    campaign.delete()
+    return redirect('campaigns:list_of_campaigns')
