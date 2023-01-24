a139ae9deb409fd9daf887727a27adc91399799f
Elias Nassar <elias.nassar.22@gmail.com>
2023-01-24 09:20:41 +0200

    redesign

--- campaigns/forms.py
+++ campaigns/forms.py
@@ -1,14 +1,39 @@
 from django.forms import ModelForm
 from .models import Campaign
-from django.db import models  
-from django.forms import fields  
-from django import forms  
+from django.db import models
+from django.forms import fields
+from django import forms
+
+
 class CampaignForm(ModelForm):
     class Meta:
+
         model = Campaign
-        fields = ['manager', 'title', 'description', 'start_date', 'end_date', 'image', 'token']
-        
-class UserImage(forms.ModelForm):  
-    class meta:  
+        fields = ['manager', 'title', 'description',
+                  'start_date', 'end_date', 'image']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control',
+                                            'style': 'width: auto',
+                                            'placeholder': 'Title',
+                                            }),
+            'description': forms.TextInput(attrs={'class': 'form-control',
+                                                  'style': 'width: auto',
+                                                  'placeholder': 'Description',}),
+            'start_date': forms.SelectDateWidget(),
+            'end_date': forms.SelectDateWidget(),
+
+        }
+
+        labels = {
+            'manager': 'Who manages this campaign?',
+            'title': '',
+            'description': '',
+            'start_date': 'Start Date: ',
+            'end_date': 'End Date: ',
+        }
+
+
+class UserImage(forms.ModelForm):
+    class meta:
         models = Campaign
-        fields = '__all__'  
+        fields = '__all__'
